1cfb937ef78ffa82883ece16e677af0a6d391df2
manhcuong2801 <le.manhcuong.hd1996.1@gmail.com>
2019-12-18 17:34:46 +0700

    hashtable

---
+++ src/MyHashNode.java
@@ -0,0 +1,153 @@
+
+import java.util.ArrayList; 
+  
+
+class MyHashNode<K, V> 
+{ 
+    K key; 
+    V value; 
+  
+ 
+    MyHashNode<K, V> next; 
+  
+     
+    public MyHashNode(K key, V value) 
+    { 
+        this.key = key; 
+        this.value = value; 
+    } 
+} 
+  
+ class Map<K, V> 
+{ 
+     private ArrayList<MyHashNode<K, V>> bucketArray; 
+  
+     private int numBuckets; 
+  
+     private int size; 
+  
+     public Map() 
+    { 
+        bucketArray = new ArrayList<>(); 
+        numBuckets = 10; 
+        size = 0; 
+  
+        // Create empty chains 
+        for (int i = 0; i < numBuckets; i++) 
+            bucketArray.add(null); 
+    } 
+  
+    public int size() { return size; } 
+    public boolean isEmpty() { return size() == 0; } 
+  
+      private int getBucketIndex(K key) 
+    { 
+        int hashCode = key.hashCode(); 
+        int index = hashCode % numBuckets; 
+        return index; 
+    } 
+  
+     public V remove(K key) 
+    { 
+         int bucketIndex = getBucketIndex(key); 
+  
+         MyHashNode<K, V> head = bucketArray.get(bucketIndex); 
+  
+         MyHashNode<K, V> prev = null; 
+        while (head != null) 
+        { 
+             if (head.key.equals(key)) 
+                break; 
+  
+              prev = head; 
+            head = head.next; 
+        } 
+  
+         if (head == null) 
+            return null; 
+  
+       size--;
+  
+        // Remove key 
+        if (prev != null) 
+            prev.next = head.next; 
+        else
+            bucketArray.set(bucketIndex, head.next); 
+  
+        return head.value; 
+    } 
+  
+ 
+    public V get(K key) 
+    { 
+         int bucketIndex = getBucketIndex(key); 
+        MyHashNode<K, V> head = bucketArray.get(bucketIndex); 
+  
+         while (head != null) 
+        { 
+            if (head.key.equals(key)) 
+                return head.value; 
+            head = head.next; 
+        } 
+  
+        // If key not found 
+        return null; 
+    } 
+  
+     public void add(K key, V value) 
+    { 
+          int bucketIndex = getBucketIndex(key); 
+        MyHashNode<K, V> head = bucketArray.get(bucketIndex); 
+  
+         while (head != null) 
+        { 
+            if (head.key.equals(key)) 
+            { 
+                head.value = value; 
+                return; 
+            } 
+            head = head.next; 
+        } 
+  
+         size++; 
+        head = bucketArray.get(bucketIndex); 
+        MyHashNode<K, V> newNode = new MyHashNode<K, V>(key, value); 
+        newNode.next = head; 
+        bucketArray.set(bucketIndex, newNode); 
+  
+         if ((1.0*size)/numBuckets >= 0.7) 
+        { 
+            ArrayList<MyHashNode<K, V>> temp = bucketArray; 
+            bucketArray = new ArrayList<>(); 
+            numBuckets = 2 * numBuckets; 
+            size = 0; 
+            for (int i = 0; i < numBuckets; i++) 
+                bucketArray.add(null); 
+  
+            for (MyHashNode<K, V> headNode : temp) 
+            { 
+                while (headNode != null) 
+                { 
+                    add(headNode.key, headNode.value); 
+                    headNode = headNode.next; 
+                } 
+            } 
+        } 
+    } 
+  
+     public static void main(String[] args) 
+    { 
+        Map<String, Integer>map = new Map<>(); 
+        map.add("this",1 ); 
+        map.add("coder",2 ); 
+        map.add("this",4 ); 
+        map.add("hi",10 ); 
+        map.add("hi",7 ); 
+        map.add("hi",5 ); 
+        System.out.println(map.size()); 
+        System.out.println(map.remove("this")); 
+        System.out.println(map.remove("this")); 
+        System.out.println(map.size()); 
+        System.out.println(map.isEmpty()); 
+    } 
+} 
